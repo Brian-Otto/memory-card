d7c66d6358b4763470866ef450a2891a2fd889d7
Brian <184952956+Brian-Otto@users.noreply.github.com>
2025-06-02 10:13:38 +0200

    Add Method to fetch pokemon data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,4 +11,25 @@ const getTestPokemon = (quantity: number) => {
   return pokemons;
 };
 
-export { getTestPokemon };
+const getPokemon = async () => {
+  const url = "https://pokeapi.co/api/v2/pokemon/1";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    console.log(
+      "The pokemon is " +
+        json.name +
+        ". Look it up here: " +
+        json.sprites.versions["generation-i"].yellow.front_default
+    );
+  } catch (e) {
+    console.log((e as Error).message);
+  }
+};
+
+export { getTestPokemon, getPokemon };
