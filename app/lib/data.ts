b95e776f6839225eef7d01dbfcd6155c45f41805
Brian <184952956+Brian-Otto@users.noreply.github.com>
2025-06-03 09:56:46 +0200

    Add randomizing of pokemon

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,10 +1,20 @@
 import Pokemon from "./Pokemon";
 
 const getPokemons = async (quantity: number) => {
+  const from = 1;
+  const to = 151;
+  const alreadySelected: number[] = [];
   const pokemons = [];
 
   for (let i = 0; i < quantity; i++) {
-    const dexNum = i + 1;
+    const dexNum = Math.floor(Math.random() * (to - from + 1)) + from;
+
+    if (alreadySelected.includes(dexNum)) {
+      i += 1;
+      continue;
+    }
+
+    alreadySelected.push(dexNum);
     const fetchedPokemon = await fetchPokemon(dexNum);
     const id = await fetchedPokemon.id;
     const lowercaseName: string = await fetchedPokemon.name;
