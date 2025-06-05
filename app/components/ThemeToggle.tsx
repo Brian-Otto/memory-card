954785b11d9954e23625a680409fb885500f835b
Brian <184952956+Brian-Otto@users.noreply.github.com>
2025-06-05 11:42:45 +0200

    Add some logic (not complete)

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -3,12 +3,22 @@ import DarkIcon from "./DarkIcon";
 import LightIcon from "./LightIcon";
 
 export default function ThemeToggle() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const getUserWantsDarkmode = () => {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  };
+
+  const [isDarkMode, setIsDarkMode] = useState(() => getUserWantsDarkmode());
+
   return (
     <>
       <button type="button" onClick={() => setIsDarkMode(!isDarkMode)}>
         {isDarkMode ? <LightIcon /> : <DarkIcon />}
       </button>
+      <p>
+        {window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "prefersDark"
+          : "noPrefersDark"}
+      </p>
     </>
   );
 }
