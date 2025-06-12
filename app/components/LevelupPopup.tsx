a64f4379eb7b71dc27e4b2ecb38f1dce4fd6ac52
Brian <184952956+Brian-Otto@users.noreply.github.com>
2025-06-12 14:32:55 +0200

    Fix unneeded fragment

--- app/components/LevelupPopup.tsx
+++ app/components/LevelupPopup.tsx
@@ -5,12 +5,10 @@ type props = {
 
 export default function LevelupPopup({ username = "you", newLevel }: props) {
   return (
-    <>
-      <div className="popup textbox bg-background text-xl md:text-3xl big-whitespace transition duration-1000">
-        {username.toUpperCase()} grew
-        <br />
-        to level {newLevel}!
-      </div>
-    </>
+    <div className="popup textbox bg-background text-xl md:text-3xl big-whitespace transition duration-1000">
+      {username.toUpperCase()} grew
+      <br />
+      to level {newLevel}!
+    </div>
   );
 }
